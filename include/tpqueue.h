a0d1dbdef3f531d1f307f95408e14c02bcffe7a9
VladVegera <94718115+VladVegera@users.noreply.github.com>
2022-06-10 17:53:48 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,22 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  private:
+    T arr[size];
+    int first;
+    int last;
+  public:
+    void push(T obj) {
+      int i = last;
+      for (; (i >= first) && arr[i % size].prior < obj.prior; --i) {
+        arr[(i+1) % size] = arr[i % size];
+      }
+      arr[(i + 1) % size] = obj;
+      ++last;
+    }
+    T pop() {
+      return arr[(first++) % size];
+    }
 };
 
 struct SYM {
